3ed40122fc971af839cdb15bcf8c852e6c7db76e
Jonathas Cunha <107582116+patocg@users.noreply.github.com>
2025-02-11 06:21:40 +0000

    Deploy, desafio curso.dev

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1><center>Em construção, em breve novidades.</center></h1>;
+    return <h1><center>A unica coisa que não podem tomar de nós é nosso conhecimento.</center></h1>;
 }
 
 export default Home;
