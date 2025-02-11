37884b63114521e12bac50dacfb324d2318bcfc6
Jonathas Cunha <107582116+patocg@users.noreply.github.com>
2025-02-11 05:12:02 +0000

    testando deploys

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return <h1><center>Em construção. Em breve novidades!!!</center></h1>;
-    return <h2><center>Amarelo, pode jogar fora, pensei que dava pra consertar ele, mas não dá.</center></h2>;    
-    return <h3><center>O decapador, guarde que pego contigo depois.</center></h3>;
+    return <h1><center>Amarelo, pode jogar fora, pensei que dava pra consertar ele, mas não dá.</center></h1>;    
+    return <h1><center>O decapador, guarde que pego contigo depois.</center></h1>;
 }
 
 export default Home;
