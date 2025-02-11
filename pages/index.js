08417ce9543f905ced8bdbc5366628712b1f81bf
Jonathas Cunha <107582116+patocg@users.noreply.github.com>
2025-02-11 05:03:06 +0000

    atualização da mensagem no site

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1><center>Página em construção. Em breve novidades!!!</center></h1>;    
+    return <h1><center>Em construção. Em breve novidades!!!</center></h1>;
+    return <h2>Amarelo, pode jogar fora, pensei que dava pra consertar ele, mas não dá.</h2>    
+    return <h3>O decapador, guarde que pego contigo depois.</h3> 
 }
 
 export default Home;
