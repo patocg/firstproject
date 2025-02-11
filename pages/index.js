0e0fe41621d0fab2faf0031dff40f422be364537
Jonathas Cunha <107582116+patocg@users.noreply.github.com>
2025-02-11 05:32:13 +0000

    testando deploys com incorporação de imagem

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
 function Home() {
-    return <h1><center>
-        Em construção. Em breve novidades!!! </br> 
-        <img=https://photos.google.com/share/AF1QipOVp3Tp-YpHOPlgCYpgARr6Lok400vMBfJYbcLMcSZ3gzfIKe-asd-tovve6Kk_cw/photo/AF1QipO1dcx-C1zibcOwzG58ZcnDy9qda2YdTuhyV3lK?key=RG0xUjFacWhKX1FQMW9saFhLMHVJQk9UcThvOEZB></img>    
-        </center></h1>;
+    return <h1><center>Em construção. Em breve novidades!!!</br>
+        <img src="https://photos.google.com/share/AF1QipOVp3Tp-YpHOPlgCYpgARr6Lok400vMBfJYbcLMcSZ3gzfIKe-asd-tovve6Kk_cw/photo/AF1QipO1dcx-C1zibcOwzG58ZcnDy9qda2YdTuhyV3lK?key=RG0xUjFacWhKX1FQMW9saFhLMHVJQk9UcThvOEZB" alt="Alicates" width="300" height="200" style="border: 1px solid #000;">
+        </center>
+        </h1>;
 
 }
 
