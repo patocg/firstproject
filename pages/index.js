7fa05f01ba26606e8df16f6ad43abaaa14463ed8
Jonathas Cunha <107582116+patocg@users.noreply.github.com>
2025-02-11 05:08:56 +0000

    msg para nelson

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return <h1><center>Em construção. Em breve novidades!!!</center></h1>;
-    return <h2>Amarelo, pode jogar fora, pensei que dava pra consertar ele, mas não dá.</h2>    
-    return <h3>O decapador, guarde que pego contigo depois.</h3> 
+    return <h2><center>Amarelo, pode jogar fora, pensei que dava pra consertar ele, mas não dá.</center></h2>;    
+    return <h3><center>O decapador, guarde que pego contigo depois.</center></h3>;
 }
 
 export default Home;
