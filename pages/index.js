73c4e53f5637b4db80f2cbff4e4dd1b2858f309b
Jonathas Cunha <107582116+patocg@users.noreply.github.com>
2025-02-11 05:14:35 +0000

    teste dos ajustes no texto da página

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
-    return <h1><center>Em construção. Em breve novidades!!!</center></h1>;
-    return <h1><center>Amarelo, pode jogar fora, pensei que dava pra consertar ele, mas não dá.</center></h1>;    
-    return <h1><center>O decapador, guarde que pego contigo depois.</center></h1>;
+    return <h1><center>Em construção. Em breve novidades!!!/n 
+        Amarelo, pode jogar fora, pensei que dava pra consertar ele, mas não dá./n
+        O decapador, guarde que pego contigo depois.</center></h1>;
 }
 
 export default Home;
