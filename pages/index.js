89020d3e8690251e67c531d422ae97ef921fd203
Jonathas Cunha <107582116+patocg@users.noreply.github.com>
2025-02-11 05:25:48 +0000

    testando deploy

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 function Home() {
-    return <h1><center>Em construção. Em breve novidades!!!/n 
-        Amarelo, pode jogar fora, pensei que dava pra consertar ele, mas não dá./n
-        O decapador, guarde que pego contigo depois.</center></h1>;
+    return <h1><center>
+        Em construção. Em breve novidades!!! </br> 
+        <img=https://photos.google.com/share/AF1QipOVp3Tp-YpHOPlgCYpgARr6Lok400vMBfJYbcLMcSZ3gzfIKe-asd-tovve6Kk_cw/photo/AF1QipO1dcx-C1zibcOwzG58ZcnDy9qda2YdTuhyV3lK?key=RG0xUjFacWhKX1FQMW9saFhLMHVJQk9UcThvOEZB></img>    
+        </center></h1>;
+
 }
 
 export default Home;
