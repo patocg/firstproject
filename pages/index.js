b835c2d728286f32165e40acf3056169c4572055
Jonathas Cunha <107582116+patocg@users.noreply.github.com>
2025-02-11 05:53:58 +0000

    corrigindo deploy

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Isso é insano <br><img src="https://lh3.googleusercontent.com/pw/AP1GczMyLSjKPQEs5FY7_nQ_ZWJp-B2JNKtYDqhihL8lRF9XniJqq94lDJfwjnktyENkHf3Smu99l8A6U_KKYfSptpkotfXSxP-z66cziGI7gohSl-ETe31clA4aUDqAojoZx8NnsJS0-8Un29pPM7OqIT87=w741-h1317-s-no-gm?authuser=0"></img></br></h1>;
+    return <h1>Em construção, volte aqui mais tarde no futuro.</h1>;
 }
 
 export default Home;
