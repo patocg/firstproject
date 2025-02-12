a7041fc517ced91a2474aaa2c0a07b3c5067caae
Jonathas Cunha <107582116+patocg@users.noreply.github.com>
2025-02-12 21:57:39 +0000

    alteração do texto do site

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1><center>A unica coisa que não podem tomar de nós é nosso conhecimento.</center></h1>;
+    return <h1><center>Quarta-Feira, 12 de Fevereiro de 2024.</center></h1>;
 }
 
 export default Home;
